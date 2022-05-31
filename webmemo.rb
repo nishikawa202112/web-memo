f74237af311ff354b1060d52d60dd78ba471a767
nishikawa <nishiyumn@jcom.zaq.ne.jp>
2022-05-31 10:57:46 +0900

    インスタンスメソッドをクラスメソッドに変更しました

--- webmemo.rb
+++ webmemo.rb
@@ -5,87 +5,98 @@ require 'csv'
 require 'cgi/util'
 
 class MemoFile
-  def read_file
-    @memo_file = CSV.read('memofile.csv')
-    @memo_file.map.with_index(0) do |line, index|
-      line.unshift(index.to_s)
+  def self.read
+    memo_file = CSV.read('memofile.csv')
+    memo_list = []
+    memo_file.each.with_index do |memo, index|
+      memo_data = {}
+      memo_data[:id] = index
+      memo_data[:title] = memo[0]
+      memo_data[:memo] = memo[1]
+      memo_list << memo_data
     end
-    @memo_file
+    memo_list
   end
 
-  def write_file(title, memo)
+  def self.write(title, memo)
     CSV.open('memofile.csv', 'a') do |file|
       file << [title, memo]
     end
   end
 
-  def edit_file(memos)
+  def self.edit(edit_id, edit_title, edit_memo)
+    memo_list = read
     CSV.open('memofile.csv', 'w') do |file|
-      memos.each do |line|
-        file << [line[1], line[2]]
+      memo_list.each do |memo|
+        if memo[:id] == edit_id
+          memo[:title] = edit_title
+          memo[:memo] = edit_memo
+        end
+        file << [memo[:title], memo[:memo]]
       end
     end
   end
 
-  def delete_file(select_id)
-    @memo_file.delete_if { |array| array[0] == select_id }
+  def self.delete(delete_id)
+    memo_list = read
+    memo_list.delete_if { |memo| memo[:id] == delete_id }
     CSV.open('memofile.csv', 'w') do |file|
-      @memo_file.each do |array|
-        file << [array[1], array[2]]
+      memo_list.each do |memo|
+        file << [memo[:title], memo[:memo]]
       end
     end
+  end
+
+  def self.find_data(find_id)
+    memo_list = read
+    memo_list.each do |memo|
+      if memo[:id] == find_id
+        @find_title = memo[:title]
+        @find_memo = memo[:memo]
+      end
+    end
+    [@find_title, @find_memo]
   end
 end
 
-memo_file = MemoFile.new
-
 get '/' do
-  @memos = memo_file.read_file
+  @memos = MemoFile.read
   erb :index
 end
 
 get '/memos' do
-  erb :newmemo
+  erb :new_memo
 end
 
 post '/memos' do
   title = escape_html(params[:title])
   memo = escape_html(params[:memo]).gsub(/\R/, "\n")
-  memo_file.write_file(title, memo)
+  MemoFile.write(title, memo)
   redirect '/'
 end
 
 get '/memos/:memo_id' do
-  @select = {}
-  @select[:id] = params[:memo_id]
-  memos = memo_file.read_file
-  @select[:title] = memos[@select[:id].to_i][1]
-  @select[:memo] = memos[@select[:id].to_i][2]
-  erb :showmemo
+  @id = params[:memo_id]
+  @title, @memo = MemoFile.find_data(@id.to_i)
+  erb :show_memo
 end
 
 get '/memos/:memo_id/edit' do
-  @select = {}
-  @select[:id] = params[:memo_id]
-  memos = memo_file.read_file
-  @select[:title] = memos[@select[:id].to_i][1]
-  @select[:memo] = memos[@select[:id].to_i][2]
-  erb :editmemo
+  @id = params[:memo_id]
+  @title, @memo = MemoFile.find_data(@id.to_i)
+  erb :edit_memo
 end
 
 patch '/memos/:memo_id' do
-  select_id = params[:memo_id]
+  id = params[:memo_id]
   title = escape_html(params[:title])
   memo = escape_html(params[:memo]).gsub(/\R/, "\n")
-  memos = memo_file.read_file
-  memos[select_id.to_i][1] = title
-  memos[select_id.to_i][2] = memo
-  memo_file.edit_file(memos)
+  MemoFile.edit(id.to_i, title, memo)
   redirect '/'
 end
 
 delete '/memos/:memo_id' do
-  select_id = params[:memo_id]
-  memo_file.delete_file(select_id)
+  id = params[:memo_id]
+  MemoFile.delete(id.to_i)
   redirect '/'
 end
