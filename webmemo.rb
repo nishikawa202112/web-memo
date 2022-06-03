05d73a30be22f8e7638fbd98f44aaf4d75dda871
nishikawa <nishiyumn@jcom.zaq.ne.jp>
2022-06-03 13:49:07 +0900

    データの扱い方等、修正しました

--- webmemo.rb
+++ webmemo.rb
@@ -5,8 +5,9 @@ require 'csv'
 require 'cgi/util'
 
 class MemoFile
+  MEMO_FILE = 'memo_file.csv'
   def self.read
-    memo_file = CSV.read('memofile.csv')
+    memo_file = CSV.read(MEMO_FILE)
     memo_list = []
     memo_file.each.with_index do |memo, index|
       memo_data = {}
@@ -19,43 +20,39 @@ class MemoFile
   end
 
   def self.write(title, memo)
-    CSV.open('memofile.csv', 'a') do |file|
+    CSV.open(MEMO_FILE, 'a') do |file|
       file << [title, memo]
     end
   end
 
-  def self.edit(edit_id, edit_title, edit_memo)
+  def self.edit(id, title, memo)
     memo_list = read
-    CSV.open('memofile.csv', 'w') do |file|
-      memo_list.each do |memo|
-        if memo[:id] == edit_id
-          memo[:title] = edit_title
-          memo[:memo] = edit_memo
+    CSV.open(MEMO_FILE, 'w') do |file|
+      memo_list.each do |data|
+        if data[:id] == id
+          data[:title] = title
+          data[:memo] = memo
         end
-        file << [memo[:title], memo[:memo]]
+        file << [data[:title], data[:memo]]
       end
     end
   end
 
-  def self.delete(delete_id)
+  def self.delete(id)
     memo_list = read
-    memo_list.delete_if { |memo| memo[:id] == delete_id }
-    CSV.open('memofile.csv', 'w') do |file|
+    memo_list.delete_if { |memo| memo[:id] == id }
+    CSV.open(MEMO_FILE, 'w') do |file|
       memo_list.each do |memo|
         file << [memo[:title], memo[:memo]]
       end
     end
   end
 
-  def self.find_data(find_id)
+  def self.find(id)
     memo_list = read
     memo_list.each do |memo|
-      if memo[:id] == find_id
-        @find_title = memo[:title]
-        @find_memo = memo[:memo]
-      end
+      return memo if memo[:id] == id
     end
-    [@find_title, @find_memo]
   end
 end
 
@@ -76,14 +73,16 @@ post '/memos' do
 end
 
 get '/memos/:memo_id' do
-  @id = params[:memo_id]
-  @title, @memo = MemoFile.find_data(@id.to_i)
+  @memo = {}
+  @memo[:id] = params[:memo_id]
+  @memo = MemoFile.find(@memo[:id].to_i)
   erb :show_memo
 end
 
 get '/memos/:memo_id/edit' do
-  @id = params[:memo_id]
-  @title, @memo = MemoFile.find_data(@id.to_i)
+  @memo = {}
+  @memo[:id] = params[:memo_id]
+  @memo = MemoFile.find(@memo[:id].to_i)
   erb :edit_memo
 end
 
